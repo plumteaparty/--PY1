623f64dfa7534ccb5785ac56018f410c920ca7e4
plumteaparty <48882848+plumteaparty@users.noreply.github.com>
2022-10-30 19:59:27 +0300

    Update task2.py

--- lab 4/task2.py
+++ lab 4/task2.py
@@ -11,9 +11,20 @@ def get_count_char(str_):
     return d
 
 
+def percent(d):
+    sum_d=0
+    for c in d:
+        sum_d += d[c]
+    for c in d:
+        d[c] = round(d[c] / sum_d * 100, 1)
+    sum_d
+    return d
+
 main_str = """
     Данное предложение будет разбиваться на отдельные слова. 
     В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список отдельных слов. 
     Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
 """
 print(get_count_char(main_str))
+print(percent(get_count_char(main_str)))
+
