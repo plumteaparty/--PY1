74f8c2fe25fa13512dfc294b44ad86b0b05fad01
plumteaparty <48882848+plumteaparty@users.noreply.github.com>
2022-11-27 20:12:53 +0300

    Update task4.py

--- lab 6/task4.py
+++ lab 6/task4.py
@@ -1,40 +1,26 @@
-OUTPUT_FILE = "output.csv"
+import json
+
+INPUT_FILE = "input.csv"
 
 
-# TODO реализовать функцию to_csv_file
-
-
-def to_csv_file(filename, headers, rows, delimiter=None, new_line=None):
+def csv_to_list_dict(filename, delimiter=None, new_line=None):
     if delimiter is None:
         delimiter = ","
     if new_line is None:
         new_line = "\n"
-    with open(filename, 'w') as f:
-        for i in range(0, len(headers)-1):
-            f.write(headers[i])
-            f.write(delimiter)
-        f.write(headers[-1])
-        f.write(new_line)
-        for a in range(0, len(rows)):
-            for b in range(0, len(headers)-1):
-                f.write(rows[a][b])
-                f.write(delimiter)
-            f.write(rows[a][-1])
-            f.write(new_line)
+    with open(filename) as f:
+        table = []
+        for i, l in enumerate(f):
+            fields = l.strip(new_line).split(delimiter)
+            if i == 0:
+                heads = fields
+                continue
+
+            table.append({})
+
+            for с, _ in enumerate(heads):
+                table[-1][heads[с]] = fields[с]
+    return table
 
 
-headers_list = ['longitude', 'latitude', 'housing_median_age', 'total_rooms', 'total_bedrooms', 'population', 'households', 'median_income', 'median_house_value']
-data = [
-    ['-122.050000', '37.370000', '27.000000', '3885.000000', '661.000000', '1537.000000', '606.000000', '6.608500', '344700.000000'],
-    ['-118.300000', '34.260000', '43.000000', '1510.000000', '310.000000', '809.000000', '277.000000', '3.599000', '176500.000000'],
-    ['-117.810000', '33.780000', '27.000000', '3589.000000', '507.000000', '1484.000000', '495.000000', '5.793400', '270500.000000'],
-    ['-118.360000', '33.820000', '28.000000', '67.000000', '15.000000', '49.000000', '11.000000', '6.135900', '330000.000000'],
-]
-
-# TODO вызвать функцию to_csv_file и записать данные в файл
-
-to_csv_file(OUTPUT_FILE, headers_list, data)
-
-with open(OUTPUT_FILE) as output_f:
-    for line in output_f:
-        print(line, end="")
+print(json.dumps(csv_to_list_dict(INPUT_FILE), indent=4))
